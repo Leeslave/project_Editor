8e36b0fa4ceef696f4ce2bade375ddfc62698335
djafladf <djafladf@naver.com>
2023-03-16 15:58:19 +0900

    dd

---
+++ Assets/Scripts/Contrast/MakingMapResult.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakingMapResult : MonoBehaviour
+{
+    public GameObject Dest1 = null;
+    public GameObject Dest2 = null;
+}
--- Assets/Scripts/Contrast/MapSub.cs
+++ Assets/Scripts/Contrast/MapSub.cs
@@ -1,10 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MapSub : MonoBehaviour
 {
     public string[] CityList;
     public GameObject CitySub;
 
+    private void Start()
+    {
+        EventTrigger eventTrigger = GetComponent<EventTrigger>();
+        if (eventTrigger == null) gameObject.AddComponent<EventTrigger>();
+        EventTrigger.Entry entry_PointerEnter = new EventTrigger.Entry();
+        entry_PointerEnter.eventID = EventTriggerType.PointerEnter;
+        entry_PointerEnter.callback.AddListener((data) => { Clicked((PointerEventData)data); });
+        eventTrigger.triggers.Add(entry_PointerEnter);
+    }
+    private void Clicked(PointerEventData data)
+    {
+        if (!CitySub.activeSelf) CitySub.SetActive(true);
+    }
 }
