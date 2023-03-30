044fda2c7038f347fc1f000309d7c5ea5ee84239
Jaehyun9912 <limit8802@gmail.com>
2023-03-30 18:16:29 +0900

    fix intro prefab

--- Assets/Scripts/MainLobby/DayIntro.cs
+++ Assets/Scripts/MainLobby/DayIntro.cs
@@ -16,9 +16,10 @@ public class DayIntro : MonoBehaviour
 
     public float textOnDelay;   // 시작부터 글자 활성화까지의 딜레이
     public float textOnDuration;    //글자 활성화 지속시간
-    public GameObject dayText;      //글자 활성화용 텍스트 오브젝트
-    private string hour;
+    public TMP_Text dayText;      //글자 활성화용 텍스트 오브젝트
+    private string hour;        // 시간대 프리셋으로 시간 문자열
 
+    /// 시간대 문자열 설정 함수 (0: 출근전, 1: 업무전, 2: 업무후, 3: 퇴근 후)
     private void setHour(int timeOffset)
     {
         switch(timeOffset) {
@@ -37,26 +38,39 @@ public class DayIntro : MonoBehaviour
         }
     }
 
+    // 컴포넌트 불러오기
     private void Awake()
     {
         awakeSFX = GetComponent<AudioSource>();
         playerDataManager = GameObject.FindObjectOfType<PlayerDataManager>();
     }
 
+    // 인트로 시작
     private void Start() 
     {
         Debug.Log("Current Renown: " + PlayerPrefs.GetInt("Renown").ToString());
         StartCoroutine("DayCountIntro");
     }    
 
+    /**
+    * 날짜 출력 인트로
+    *   - 날짜 텍스트 출력
+    *   - Intro 비활성화 하면서 현재 WorldCanvas 설정하는 함수 호출
+    */
     IEnumerator DayCountIntro() {
-        dayText.SetActive(false);
+        dayText.gameObject.SetActive(false);
+
+        // 텍스트 세팅
         setHour(PlayerPrefs.GetInt("Time"));
-        dayText.GetComponent<TMP_Text>().text = $"제국력 {PlayerPrefs.GetInt("Year")}년 {PlayerPrefs.GetInt("Month")}월 {PlayerPrefs.GetInt("Day")}일\n\n{hour}";
+        dayText.text = $"제국력 {PlayerPrefs.GetInt("Year")}년 {PlayerPrefs.GetInt("Month")}월 {PlayerPrefs.GetInt("Day")}일\n\n{hour}";
         yield return new WaitForSeconds(textOnDelay);
-        dayText.SetActive(true);
-        awakeSFX.Play();
+
+        //날짜 활성화 애니메이션, 효과음
+        dayText.gameObject.SetActive(true);
+        if(awakeSFX != null) awakeSFX.Play();
         yield return new WaitForSeconds(textOnDuration);
+
+        //종료 및 WorldCanvas 설정
         gameObject.SetActive(false);
            // TODO: 위치에 해당하는 worldCanvas 생성으로 수정
     }
