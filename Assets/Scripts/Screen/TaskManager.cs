8093db28f400a9b26efa5a009a01ae546a68af7d
Jaehyun9912 <limit8802@gmail.com>
2023-05-03 15:21:38 +0900

    task 애니메이션 진입 작업

--- Assets/Scripts/Screen/TaskManager.cs
+++ Assets/Scripts/Screen/TaskManager.cs
@@ -7,11 +7,23 @@ public class TaskManager : MonoBehaviour
     /**
     * 업무 지원 프로그램 스크립트
     *   - 업무 안내 콘솔 기능
-    *   - 해당하는 업무 실행
+    *   - 해당 업무 실행
     *   - 업무 결과 돌려받기
     *   - 하루 업무 클리어
     */
 
-    AnimationController animationController;
+    public AnimationController taskConsoleAnimation;
+    public bool taskClear = false;
     
+    private void Awake()
+    {
+        taskConsoleAnimation.anims[0].Clear();
+        taskConsoleAnimation.Play(0);
+    }
+
+    private void OnEnabled()
+    {
+        taskConsoleAnimation.anims[0].Clear();
+        taskConsoleAnimation.Play(0);
+    }
 }
