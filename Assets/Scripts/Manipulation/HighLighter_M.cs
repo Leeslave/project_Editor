b7902879c678a20c40d11c736dc6b084b9dda7c5
djafladf <djafladf@naver.com>
2023-05-20 23:20:53 +0900

    test1

---
+++ Assets/Scripts/Manipulation/HighLighter_M.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class HighLighter_M : MonoBehaviour
+{
+    public InfChange In;
+    public Image Clicker;
+    public int IsFolder;    // File : 0, Folder : 1
+
+    private bool Selected = false;
+    private Color ColorOn;
+    private Color ColorHigh;
+    private Color ColorOff;
+
+    [HideInInspector]
+    public List<string> Files;
+    private void Awake()
+    {
+        if (IsFolder == 1)
+        {
+            Files = new List<string>();
+            MyUi.textReader("Manipulation/" + name, ref Files);
+        }
+        ColorOff = Clicker.color;
+        ColorHigh = Clicker.color + new Color(0, 0, 0, 0.5f);
+        ColorOn = Clicker.color + new Color(0, 0, 0, 1);
+        MyUi.ButtonInit(GetComponent<EventTrigger>(),OnPointer,OutPointer,Touch);
+    }
+
+    private void OnPointer(PointerEventData Event)
+    {
+        if(!Selected)Clicker.color = ColorHigh;
+    }
+
+    private void OutPointer(PointerEventData Event)
+    {
+        if(!Selected) Clicker.color = ColorOff;
+    }
+
+    private void Touch(PointerEventData Event)
+    {
+        Clicker.color = ColorOn;
+        Selected = true;
+        In.TouchManager(gameObject,IsFolder);
+    }
+
+    public void HighLightOff()
+    {
+        Selected = false;
+        OutPointer(null);
+    }
+}
--- Assets/Scripts/Manipulation/InfChange.cs
+++ Assets/Scripts/Manipulation/InfChange.cs
@@ -1,11 +1,7 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using System.IO;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
-using Unity.VisualScripting;
 
 public class InfChange : MonoBehaviour
 {
@@ -15,26 +11,75 @@ public class InfChange : MonoBehaviour
     public TMP_Text Country;
     public TMP_Text Job;
     public TMP_Text TerminalFolder;
+    public Image Face;
     public GameObject Files;
     public GameObject Folders;
-    public GameObject ViewContent;
-    public UnityEngine.UI.Image Face;
 
-    private GameObject CurDetail;
-    private GameObject CurFile;
-    private GameObject CurObject;
-
-    private float InputLag = 0.5f;
-    private string CurFolder = "Job";
-    private List<string> Jobs;
-    private List<string> Countries;
+    private GameObject CurFolder = null;
+    private HighLighter_M CurHighLight = null;
+    private GameObject CntObject = null;
 
     private List<List <string>> CommandList_Back = new List<List<string>>();    // »ý°¢Á» ÇØº½
     private List<List<string>> CommandList_Go = new List<List<string>>();
 
-    public void TouchManager()
+    public void TouchManager(GameObject cnt, int Type)
     {
+        switch (Type)
+        {
+            case 0:             // File
+                if (CurFolder == null)
+                {
+                    CurFolder = cnt;
+                    CurHighLight = CurFolder.GetComponent<HighLighter_M>();
+                }
+                else if (CurFolder != cnt)
+                {
+                    CurHighLight.HighLightOff();
+                    CurFolder = cnt;
+                    CurHighLight = CurFolder.GetComponent<HighLighter_M>();
+                }
+                else
+                {
+                    CurHighLight.HighLightOff();
+                    CurFolder = null;
+                }
+                break;
+            case 1:             // Folder
+                if (CurFolder == null)
+                {
+                    CurFolder = cnt;
+                    CurHighLight = CurFolder.GetComponent<HighLighter_M>();
+                }
+                else if (CurFolder != cnt)
+                {
+                    CurHighLight.HighLightOff();
+                    CurFolder = cnt;
+                    CurHighLight = CurFolder.GetComponent<HighLighter_M>();
+                }
+                else
+                {
+                    CurHighLight.HighLightOff();
+                    Folders.SetActive(false);
+                    Files.SetActive(true);
+                    OpenFolder();
+                    CurFolder = null;
+                }
+                break;
+            case 2:             // Etc
+                break;
+        }
 
+    }
+
+    private void OpenFolder()
+    {
+        GameObject cnt;
+        for(int i = 0; i < CurHighLight.Files.Count; i++)
+        {
+            cnt = Files.transform.GetChild(i).gameObject;
+            cnt.SetActive(true);
+            cnt.transform.GetChild(2).GetComponent<TMP_Text>().text = CurHighLight.Files[i];
+        }
     }
 
     public void ChangeInf(PeopleIndex FindPeople)
