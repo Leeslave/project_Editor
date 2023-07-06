da7a8bf7e8b602f7dba75f4ac5e5f9bf743ed501
Jaehyun9912 <limit8802@gmail.com>
2023-07-06 16:25:16 +0900

    paragraph 신 클래스 구축

---
+++ Assets/Scripts/Chat/Paragraph.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Paragraph
+{
+    // 대사 타입
+    string type;
+    string[] text;
+    int fontSize;
+    int delay;
+
+}
