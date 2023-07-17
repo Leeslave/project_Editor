b0808ff4481822a5db9c873156006f47de07053a
Ozeco-Mmem <limit8802@gmail.com>
2023-07-17 16:26:19 +0900

    선택지, 일반 반응 함수 추가

--- Assets/Scripts/Chat/Chat.cs
+++ Assets/Scripts/Chat/Chat.cs
@@ -47,17 +47,16 @@ public class Chat : MonoBehaviour
     private int index;   // 현재 대화 인덱스
 
     /// 이벤트
-    private UnityEvent normalEvent = new UnityEvent();
-    private UnityEvent[] choiceEvents = new UnityEvent[3];
+    private UnityEvent<int> normalEvent = new UnityEvent<int>();    // 대사 이벤트
+    private int normalEventParam;       //매개변수
+    private List<UnityEvent<int>> choiceEvents = new List<UnityEvent<int>>(3);    // 선택지 이벤트
+    private List<int> choiceEventParam = new List<int>(3);        // 매개변수
 
     private List<Paragraph> paragraphs; // 대화 리스트
 
     /// 싱글턴 선언
     private static Chat _instance;
-    public static Chat Instance
-    {
-        get { return _instance; }
-    }
+    public static Chat Instance { get { return _instance; } }
     public void Awake()
     {
         if(!_instance)
@@ -68,9 +67,10 @@ public class Chat : MonoBehaviour
             index = -1;
 
             // 이벤트 초기화
-            for(int i = 0; i<3; i++)
+            for(int i = 0; i<choiceEvents.Capacity; i++)
             {
-                choiceEvents[i] = new UnityEvent();
+                choiceEvents.Add(new UnityEvent<int>());
+                choiceEventParam.Add(-1);
             }
 
             StartChat("Henderson");         ///디버깅
@@ -92,7 +92,8 @@ public class Chat : MonoBehaviour
         * 대화
         - chatName으로 파일 불러오고 Chat 활성화
         */
-        string fileName = $"{chatName}_{GameSystem.Instance.player.dateIndex}_{GameSystem.Instance.player.time}";    // debug
+        string fileName = $"{chatName}_{GameSystem.Instance.player.dateIndex}_{GameSystem.Instance.player.time}";   
+        Debug.Log($"대화 시작 : {fileName}"); 
         isTalk = LoadChatFile(fileName);
         if (isTalk == false)
             return;
@@ -133,123 +134,10 @@ public class Chat : MonoBehaviour
 
         SetChatUI(paragraph.type);  // 대사 타입에 따라 UI 설정
         
+        // 대사 애니메이션 실행
         if (paragraph.type != Paragraph.TalkType.choice)
         {
             Coroutine textAnimation = StartCoroutine(TextAnimation(paragraph as NormalParagraph));
-        }
-    }
-
-    /// 대사 타입에 따라 UI 설정
-    private void SetChatUI(Paragraph.TalkType talkType)
-    {
-        switch(talkType)
-        {
-            /// 선택지 상태일때
-            case Paragraph.TalkType.choice:
-                ChoiceParagraph choiceParagraph = paragraphs[index] as ChoiceParagraph;
-
-                /// 캐릭터 CG 활성화
-                choicePanel.SetActive(true);    // 선택지 패널 활성화
-                optionPanel.SetActive(false);   // 옵션 패널 비활성화
-                if (choiceParagraph == null)
-                {
-                    Debug.Log("선택지 없음");
-                    return;
-                }
-
-                GameObject choiceButton;    // 할당할 버튼 iterator
-                switch(choiceParagraph.choices.Count)
-                {   
-                    // 선택지 1개일때 (가운데 2번 사용)
-                    case 1:
-                        choicePanel.transform.GetChild(0).gameObject.SetActive(false);  // 1번 선택지 비활성화
-                        choiceButton = choicePanel.transform.GetChild(1).gameObject;    // 2번 선택지 선택
-                        choicePanel.transform.GetChild(2).gameObject.SetActive(false);  // 3번 선택지 비활성화
-
-                        choiceButton.transform.GetChild(0).gameObject.GetComponent<TMP_Text>().text = choiceParagraph.choices[0].text;  // 선택지 텍스트 설정
-                        SetEventListener(choiceEvents[1], choiceParagraph.action);      // 이벤트 함수 설정
-                        choiceButton.SetActive(true);   //버튼 활성화
-                        break;
-                    // 선택지 2개일때 (위, 아래 1,3번 사용)
-                    case 2:
-                        // 1번 선택지 설정
-                        choiceButton = choicePanel.transform.GetChild(0).gameObject;
-                        choiceButton.transform.GetChild(0).gameObject.GetComponent<TMP_Text>().text = choiceParagraph.choices[0].text;
-                        SetEventListener(choiceEvents[0], choiceParagraph.action);
-                        choiceButton.SetActive(true);
-
-                        // 3번 선택지 설정
-                        choiceButton = choicePanel.transform.GetChild(2).gameObject;
-                        choiceButton.transform.GetChild(0).gameObject.GetComponent<TMP_Text>().text = choiceParagraph.choices[1].text;
-                        SetEventListener(choiceEvents[2], choiceParagraph.action);
-                        choiceButton.SetActive(true);
-                        
-                        choicePanel.transform.GetChild(1).gameObject.SetActive(false);  // 2번 선택지 비활성화
-                        break;
-                    // 선택지 3개일때
-                    case 3:
-                        // 첫번째 버튼 설정
-                        choiceButton = choicePanel.transform.GetChild(0).gameObject;
-                        choiceButton.transform.GetChild(0).gameObject.GetComponent<TMP_Text>().text = choiceParagraph.choices[0].text;
-                        SetEventListener(choiceEvents[0], choiceParagraph.action);
-                        choiceButton.SetActive(true);
-                        // 두번째 버튼 설정
-                        choiceButton = choicePanel.transform.GetChild(1).gameObject;
-                        choiceButton.transform.GetChild(0).gameObject.GetComponent<TMP_Text>().text = choiceParagraph.choices[1].text;
-                        SetEventListener(choiceEvents[1], choiceParagraph.action);
-                        choiceButton.SetActive(true);
-                        // 세번째 버튼 설정
-                        choiceButton = choicePanel.transform.GetChild(2).gameObject;
-                        choiceButton.transform.GetChild(0).gameObject.GetComponent<TMP_Text>().text = choiceParagraph.choices[2].text;
-                        SetEventListener(choiceEvents[2], choiceParagraph.action);
-                        choiceButton.SetActive(true);
-                        break;
-                }
-                break;
-
-            /// 컷씬 상태일때
-            case Paragraph.TalkType.cutScene:
-                NormalParagraph cutSceneParagraph = paragraphs[index] as NormalParagraph;
-                background.sprite = GetSprite(cutSceneParagraph.background);    // 배경 이미지 설정 
-                background.gameObject.SetActive(true);      // 배경 이미지 활성화
-
-                /// 캐릭터 CG 활성화
-                choicePanel.SetActive(false);    // 선택지 패널 비활성화
-                optionPanel.SetActive(true);   // 옵션 패널 활성화 
-                // 대사 존재시 대사창 활성화
-                if (cutSceneParagraph.text != "")  
-                {
-                    talkPanel.SetActive(true);
-                    talkerName.text = cutSceneParagraph.talker;     // 발화자 이름
-                    talkerInfo.text = cutSceneParagraph.talkerInfo; // 발화자 설명
-
-                    text.fontSize = cutSceneParagraph.fontSize;   // 대사 크기 설정
-                    text.text = "";             // 대사 초기화
-                }
-                else
-                    talkPanel.SetActive(false);  
-
-                SetEventListener(normalEvent, cutSceneParagraph.action);
-                break;
-            
-            /// 일반 대화 상태일때
-            case Paragraph.TalkType.talk:
-                NormalParagraph normalParagraph = paragraphs[index] as NormalParagraph;
-
-                /// 캐릭터 CG 활성화
-                background.gameObject.SetActive(false); // 배경 비활성화
-                choicePanel.SetActive(false);   // 선택지 패널 비활성화
-                optionPanel.SetActive(true);    // 옵션 패널 활성화 
-                talkPanel.SetActive(true);      // 대화 패널 활성화
-
-                talkerName.text = normalParagraph.talker;     // 발화자 이름
-                talkerInfo.text = normalParagraph.talkerInfo; // 발화자 설명
-
-                text.fontSize = normalParagraph.fontSize;   // 대사 크기 설정
-                text.text = "";             // 대사 초기화
-
-                SetEventListener(normalEvent, normalParagraph.action);
-                break;
         }
     }
 
@@ -272,18 +160,161 @@ public class Chat : MonoBehaviour
     public void OnChoicePressed(int num)
     {   
         // 선택지 번호 오류
-        if ( (num >= 3) || (num < 0) )
+        if ( (num >= choicePanel.transform.childCount) || (num < 0) )
             return;
 
-        if (choiceEvents[num].GetPersistentEventCount() != 0)
-            choiceEvents[num].Invoke();
+        choiceEvents[num].Invoke(choiceEventParam[num]);    // 반응 함수 실행
+        NextChat();
+    }
+
+    /// 변수 텍스트 적용
+    private string GetVariableValue(string variableName)
+    {
+        switch(variableName)
+        {
+            case "year":
+                return GameSystem.Instance.todayData.date.year.ToString();
+            case "month":
+                return GameSystem.Instance.todayData.date.month.ToString();
+            case "day":
+                return GameSystem.Instance.todayData.date.day.ToString();
+        }
+        return "";
+    }
+
+    /// 함수코드로 이벤트 리스터 할당
+    public void SetAction(UnityEvent<int> _event, string func)
+    {
+        // 반응 함수 없을 시 할당 안함
+        if (func == null)
+        {
+            return;
+        }
+
+        /// 반응 함수 할당
+        switch(func)
+        {
+            case "Jump":
+                _event.AddListener((int num) => { index = num - 1; });
+                break;
+        }
+    }
+
+    /// 선택지 할당
+    public void SetChoice(int choiceNum, Choice choice = null)
+    {
+        // 선택지 버튼 오류
+        if (choiceNum >= choicePanel.transform.childCount || choiceNum < 0)
+            return;
+
+        // 해당 선택지 버튼
+        GameObject button = choicePanel.transform.GetChild(choiceNum).gameObject;
+
+        // 선택지 미사용시 비활성화
+        if (choice == null)
+        {
+            button.SetActive(false);                        
+            return;
+        }   
+
+        button.transform.GetChild(0).gameObject.GetComponent<TMP_Text>().text = choice.text;  // 선택지 텍스트 설정
+
+        choiceEvents[choiceNum].RemoveAllListeners();           // 반응 초기화
+        SetAction(choiceEvents[choiceNum], choice.reaction);     // 선택지 반응 설정
+        choiceEventParam[choiceNum] = choice.reactionParam;      // 선택지 반응 매개변수 설정   
+
+        button.SetActive(true);     // 선택지 활성화
+    }
+
+    /// 대사 타입에 따라 UI 설정
+    private void SetChatUI(Paragraph.TalkType talkType)
+    {
+        switch(talkType)
+        {
+            /// 선택지 상태일때
+            case Paragraph.TalkType.choice:
+                ChoiceParagraph choiceParagraph = paragraphs[index] as ChoiceParagraph;
+
+                /// TODO: 캐릭터 CG 활성화
+                choicePanel.SetActive(true);    // 선택지 패널 활성화
+                optionPanel.SetActive(false);   // 옵션 패널 비활성화
+
+                switch(choiceParagraph.choices.Count)
+                {   
+                    // 선택지 1개일때 (가운데 2번 사용)
+                    case 1:
+                        SetChoice(0);                               // 1번 선택지 비활성화
+                        SetChoice(1, choiceParagraph.choices[0]);   // 2번 선택지 설정
+                        SetChoice(2);                               // 3번 선택지 비활성화
+                        break;
+                    // 선택지 2개일때 (위, 아래 1,3번 사용)
+                    case 2:
+                        SetChoice(0, choiceParagraph.choices[0]);   // 1번 선택지 설정
+                        SetChoice(1);                               // 2번 선택지 비활성화
+                        SetChoice(2, choiceParagraph.choices[1]);   // 3번 선택지 설정
+                        break;
+                    // 선택지 3개일때
+                    case 3:
+                        SetChoice(0, choiceParagraph.choices[0]);   // 1번 선택지 설정
+                        SetChoice(1, choiceParagraph.choices[1]);   // 2번 선택지 설정
+                        SetChoice(2, choiceParagraph.choices[2]);   // 3번 선택지 설정
+                        break;
+                }
+                break;
+
+            /// 컷씬 상태일때
+            case Paragraph.TalkType.cutScene:
+                NormalParagraph cutSceneParagraph = paragraphs[index] as NormalParagraph;
+
+                background.sprite = GetSprite(cutSceneParagraph.background);    // 배경 이미지 설정 
+                background.gameObject.SetActive(true);      // 배경 이미지 활성화
+
+                /// TODO: 캐릭터 CG 활성화
+                choicePanel.SetActive(false);    // 선택지 패널 비활성화
+                optionPanel.SetActive(true);   // 옵션 패널 활성화 
+
+                // 대사 존재시 대사창 활성화
+                if (cutSceneParagraph.text != "")  
+                {
+                    talkPanel.SetActive(true);
+                    talkerName.text = cutSceneParagraph.talker;     // 발화자 이름
+                    talkerInfo.text = cutSceneParagraph.talkerInfo; // 발화자 설명
+
+                    text.fontSize = cutSceneParagraph.fontSize;   // 대사 크기 설정
+                    text.text = "";             // 대사 초기화
+                }
+                else
+                    talkPanel.SetActive(false);  
+                break;
+            
+            /// 일반 대화 상태일때
+            case Paragraph.TalkType.talk:
+                NormalParagraph normalParagraph = paragraphs[index] as NormalParagraph;
+
+                /// 캐릭터 CG 활성화
+                background.gameObject.SetActive(false); // 배경 비활성화
+                choicePanel.SetActive(false);   // 선택지 패널 비활성화
+                optionPanel.SetActive(true);    // 옵션 패널 활성화 
+                talkPanel.SetActive(true);      // 대화 패널 활성화
+
+                talkerName.text = normalParagraph.talker;     // 발화자 이름
+                talkerInfo.text = normalParagraph.talkerInfo; // 발화자 설명
+
+                text.fontSize = normalParagraph.fontSize;   // 대사 크기 설정
+                text.text = "";             // 대사 초기화
+
+                break;
+        }
+
+        SetAction(normalEvent, paragraphs[index].action);    // 반응 설정
+        normalEventParam = paragraphs[index].actionParam;           // 반응 매개변수 설정
     }
 
     /// 대사 출력 애니메이션
     IEnumerator TextAnimation(NormalParagraph paragraph)
     {
         /**
-        * BGM 실행
+        * SFX 실행
         * 대사 출력
             - delay
             - 변수값 적용
@@ -305,29 +336,9 @@ public class Chat : MonoBehaviour
         for (int i = 0; i < paragraph.text.Length; i++)
         {
             text.text += paragraph.text[i];
-            yield return new WaitForSeconds(paragraph.delay / 10);
+            /// TODO: 텍스트 효과음 출력
+            yield return new WaitForSeconds(paragraph.textDelay / 10);
         }
-    }
-
-    /// 함수코드로 이벤트리스터 할당
-    public void SetEventListener(UnityEvent _event, string func)
-    {
-
-    }
-
-    /// 변수 텍스트 적용
-    private string GetVariableValue(string variableName)
-    {
-        switch(variableName)
-        {
-            case "year":
-                return GameSystem.Instance.todayData.date.year.ToString();
-            case "month":
-                return GameSystem.Instance.todayData.date.month.ToString();
-            case "day":
-                return GameSystem.Instance.todayData.date.day.ToString();
-        }
-        return "";
     }
 
     /// 스프라이트 파일 불러오기
--- Assets/Scripts/Chat/Paragraph.cs
+++ Assets/Scripts/Chat/Paragraph.cs
@@ -9,14 +9,15 @@ public class Paragraph
     {
         /// 대화 타입
         talk,   // 일반 대화
-        choice, // 
-        cutScene
+        choice, // 선택지 대화
+        cutScene // 컷씬 대화
     }
     public TalkType type;    // 대사 타입
-    public List<CharacterCG> characters;  // 캐릭터 CG 리스트
-    public string background;  // 배경 이미지
-    public string bgm;         // 배경음악
-    public string action;  // 대화 후 이벤트
+    public List<CharacterCG> characters = new List<CharacterCG>();  // 캐릭터 CG 리스트
+    public string background = null;  // 배경 이미지
+    public string bgm = null;         // 배경음악
+    public string action = null;    // 대화 후 반응
+    public int actionParam = -1;    // 반응 매개변수
 }
 
 [System.Serializable]
@@ -32,30 +33,15 @@ public class NormalParagraph : Paragraph
     public string talker;  // 발화자
     public string talkerInfo;    // 발화자 설명
     public string text;  // 내용
-    public List<VariableReplace> variables; // 변수값
-    public int fontSize;   // 글자 크기
-    public float delay;      // 텍스트간 딜레이
+    public List<VariableReplace> variables = new List<VariableReplace>(); // 변수값
+    public int fontSize = 16;   // 글자 크기
+    public float textDelay = 0.3f;      // 텍스트간 딜레이
 
     [System.Serializable]
     public class VariableReplace
     {
-        public string keyword;
-        public string variableName;
-    }
-
-    public NormalParagraph()
-    {
-        type = TalkType.talk;
-        characters = new List<CharacterCG>();
-        background = null;
-        bgm = null;
-        action = null;
-        talker = "";
-        talkerInfo = "";
-        text = "";
-        variables = new List<VariableReplace>();
-        fontSize = 16;
-        delay = 0.3f;
+        public string keyword;      // 텍스트상의 변수 키워드
+        public string variableName; // 해당하는 변수코드명
     }
 }   
 
@@ -63,7 +49,7 @@ public class NormalParagraph : Paragraph
 [System.Serializable]
 public class ChoiceParagraph : Paragraph
 {
-    public List<Choice> choices;
+    public List<Choice> choices = null; // 선택지들 리스트
 }
 
 
@@ -77,12 +63,12 @@ public class Choice
     {
         /// 선택지 타입
         normal,
-        
+        Ending
     }
-    public ChoiceType type;
-    public string text;
-    public string reaction;
-    
+    public ChoiceType type = ChoiceType.normal;  // 선택지 타입
+    public string text;     //선택지 텍스트
+    public string reaction = null;      //선택지 반응
+    public int reactionParam = -1;       // 선택지 반응 매개변수
 }
 
 /// 캐릭터 CG
@@ -90,8 +76,8 @@ public class Choice
 public class CharacterCG
 {
     public string fileName;    // 캐릭터 파일명
-    public string position; // 왼쪽, 가운데, 오른쪽 CG 위치
-    public int emotion;    // 캐릭터 표정 번호
-    public int pose;       // 캐릭터 자세 번호
-    public bool isHighlight;
+    public string position = "Center"; // 왼쪽, 가운데, 오른쪽 CG 위치
+    public int emotion = 0;    // 캐릭터 표정 번호
+    public int pose = 0;       // 캐릭터 자세 번호
+    public bool isHighlight = true; // 캐릭터 하이라이트
 }
