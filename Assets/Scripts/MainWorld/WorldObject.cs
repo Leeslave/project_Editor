d28e3bd2f2bbc6b4ee5702f4b4a55c57e170bdc6
jaehyun9912 <limit8802@gmail.com>
2024-02-24 15:28:30 +0900

    WorldObject 적용 중

--- Assets/Scripts/MainWorld/WorldObject.cs
+++ Assets/Scripts/MainWorld/WorldObject.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class WorldObject : MonoBehaviour
+public abstract class WorldObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int type; // 상호작용 타입
+    public int count = 0;   // 상호작용 가능 횟수 (0 : 무제한)
+
+    // 오브젝트 클릭 시
+    public virtual void ObjectClicked()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    // 오브젝트 활성화 시
+    public virtual void ObjectActive()
     {
-        
+
+    }
+
+    void Awake()
+    {
+        // 버튼 이벤트 할당
+        if (TryGetComponent(out Button bt))
+        {
+            bt.onClick.AddListener(ObjectClicked);
+        }
     }
 }
