5d2051091375452ca853ac9be6e724ff927418ef
Jaehyun9912 <limit8802@gmail.com>
2024-05-19 03:40:02 +0900

    Revert "Merge branch 'develop' into feature/ScreenUI"
    This reverts commit d889b0915c754b35f63be73e378982ae884beb6a, reversing
    changes made to 6575c3c8e13db51dcfaa17f45608488395cb131f.

--- Assets/Scripts/Screen/MsgManager.cs
+++ Assets/Scripts/Screen/MsgManager.cs
@@ -34,7 +34,6 @@ public class MsgManager : SingletonObject<MsgManager>
         // 메시지 패널 생성
         for(int i = 0; i < messageDatas.Count; i++)
         {
-            Debug.Log(messageDatas.Count);
             GameObject newPanel = Instantiate(MsgPrefab, MsgListPanel);
             newPanel.transform.GetChild(0).GetComponent<TMP_Text>().text = messageDatas[i].name;
             MsgListPanel.sizeDelta += new Vector2(0, panelSize);
@@ -112,7 +111,6 @@ public class MsgManager : SingletonObject<MsgManager>
         }
 
         MsgPanel.sizeDelta -= new Vector2(0, MsgPanel.sizeDelta.y);
-        MsgPanel.position = new Vector3(MsgPanel.position.x, 0, MsgPanel.position.z);
     }
     
     /// <summary>
