017f2ba6fd6a36d0db44f5ddd03141ad78a6c816
Jaehyun9912 <limit8802@gmail.com>
2024-09-20 00:15:46 +0900

    스크린 버튼 오류 해결

--- Assets/Scripts/MainWorld/ScreenButton.cs
+++ Assets/Scripts/MainWorld/ScreenButton.cs
@@ -8,15 +8,6 @@ public class ScreenButton : MonoBehaviour
     *   - 스크린 아이콘을 눌러 스크린을 활성화
     *   - down 버튼을 눌러 스크린 비활성화
     */
-    public Button downButton;   // 취소 버튼 (아래 버튼)
-
-    private void OnEnable() {
-        downButton.gameObject.SetActive(true);
-    }
-
-    private void OnDisable() {
-        downButton.gameObject.SetActive(false);
-    }
 
     /// <summary>
     /// 스크린을 눌러 확대 or 스크린 키기
