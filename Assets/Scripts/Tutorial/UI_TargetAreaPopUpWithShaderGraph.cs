7a523bfddbf77ec2ea880eb429c1ebb509881aaa
prayslaks <prayslaks@naver.com>
2025-07-04 13:42:13 +0900

    2025-07-04
    튜토리얼에 강조용 서클 셰이더를 이식하여 시인성을 개선하였음.

--- Assets/Scripts/Tutorial/UI_TargetAreaPopUpWithShaderGraph.cs
+++ Assets/Scripts/Tutorial/UI_TargetAreaPopUpWithShaderGraph.cs
@@ -6,23 +6,25 @@ public class UI_TargetAreaPopUpWithShaderGraph : MonoBehaviour
 {
     private Image Background { get; set; }
     private TMP_Text[] Instruction { get; set; }
+    private FocusCircleController focusCircleController { get; set; }
 
     private void Awake()
     {
         Background = transform.GetChild(0).GetComponent<Image>();
         Instruction = transform.GetChild(2).GetComponentsInChildren<TMP_Text>();
-        
-        //비가시
-        Background.color = Color.clear;
-        foreach(TMP_Text i in Instruction)
-            i.color = Color.clear;
+        focusCircleController = transform.GetChild(3).GetChild(0).GetComponent<FocusCircleController>();
     }
 
     private void OnEnable()
     {
+        //초기화
+        Background.color = Color.clear;
+        foreach (TMP_Text i in Instruction)
+            i.color = Color.clear;
+
         //가시
         LJWConverter.Instance.GradientImageColor(false, 0f, 1f, new Color(0f, 0f, 0f, 0.95f), Background);
-        foreach(TMP_Text i in Instruction)
+        foreach (TMP_Text i in Instruction)
             LJWConverter.Instance.GradientTMPColor(false, 1f, 1f, Color.white, i);
     }
 }
